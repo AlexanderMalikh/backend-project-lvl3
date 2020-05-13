c29072b1b8f907c995105569caf420f2a5563461
Alexander Malykh <fulgrimka1@gmail.com>
2020-05-13 23:59:24 +0300

    added 1 test case

--- __tests__/test.loader.js
+++ __tests__/test.loader.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment node
+ */
+
 import nock from 'nock';
 import path from 'path';
 import os from 'os';
@@ -11,12 +15,21 @@ let tempDirName = '';
 beforeEach(async () => {
   tempDirName = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
 });
-
-test('loader', async () => {
-  console.log(tempDirName);
+test('http requests is ok', async () => {
   const nockData = await fs.readFile(getFixturePath('nock'), 'utf-8');
   const scope = nock('https://ru.hexlet.io')
     .get('/my')
     .reply(200, nockData);
-  load('https://ru.hexlet.io/my', tempDirName).then(() => expect(scope.isDone()).toBe(true));
+  await load('https://ru.hexlet.io/my', tempDirName);
+  expect(scope.isDone()).toBe(true);
+});
+
+test('html file created', async () => {
+  const nockData = await fs.readFile(getFixturePath('nock'), 'utf-8');
+  nock('https://ru.hexlet.io')
+    .get('/my')
+    .reply(200, nockData);
+  await load('https://ru.hexlet.io/my', tempDirName);
+  const stat = await fs.lstat(path.join(tempDirName, 'ru-hexlet-io-my.html'));
+  return expect(stat.isFile()).toBe(true);
 });
