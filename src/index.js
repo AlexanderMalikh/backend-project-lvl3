d0b40dfbb809fc1af813a7a75f12b9999678c8b8
Alexander Malykh <fulgrimka1@gmail.com>
2020-05-21 11:19:40 +0300

    ref

--- src/index.js
+++ src/index.js
@@ -1,72 +1,58 @@
-/* eslint no-unused-expressions: ["error", { "allowTernary": true }] */
 const path = require('path');
-const url = require('url');
 const fs = require('fs').promises;
 const axios = require('axios');
-const cheerio = require('cheerio');
+const { parseTags, parseURL } = require('./parsers.js');
 
-const createFilenameByUrl = (webPageUrl) => {
-  const parts = webPageUrl.replace(/[^A-Za-zА-Яа-яЁё0-9]/g, '-').split('-').slice(3);
+const createFilenameByUrl = (webPageUrl, ext = '') => {
+  const parts = webPageUrl.replace('https://', '')
+    .replace(ext, '')
+    .replace(/[^A-Za-zА-Яа-яЁё0-9]/g, '-')
+    .split('-');
   const filename = parts.reduce((acc, item) => (acc === '' ? `${item}` : `${acc}-${item}`), '');
   return filename;
 };
 
-const mapp = {
-  script: 'script[src]',
-  img: 'img[src]',
-  link: 'link[href]',
+const getFilenameExtensionFromUrl = (url) => {
+  const tmp = url.substr(1 + url.lastIndexOf('/'));
+  const file = tmp.split('?')[0].split('#')[0];
+  let ext = '';
+
+  if (file.lastIndexOf('.') === -1) {
+    ext = '.html';
+  } else ext = file.substr(file.lastIndexOf('.') + 1);
+
+  const filename = createFilenameByUrl(new URL(url).pathname, ext);
+  console.log(filename, ext);
+  return `${filename}.${ext}`;
 };
 
-const tagsWithResources = ['script', 'img', 'link'];
-
-const parseTags = (pathToHtml) => {
-  const linksArr = [];
-  return fs.readFile(pathToHtml, 'utf-8')
-    .then((data) => {
-      const $ = cheerio.load(data);
-      tagsWithResources.map((tag) => {
-        $(`${mapp[tag]}`).each((i, el) => {
-          tag === 'link' ? linksArr.push($(el).attr('href')) : linksArr.push($(el).attr('src'));
-        });
-        return tag;
-      });
-      return linksArr;
-    })
-    .catch((err) => console.log(err));
-};
-
-const parseURL = (links, webPageUrl) => {
-  const parsedURLS = links.filter((link) => new URL(link, webPageUrl).host === webPageUrl.host)
-    .map((link) => new URL(link, webPageUrl).href);
-  console.log('LINKS_______________________________\n', parsedURLS);
-
-  return parsedURLS;
-};
-/*
-const dowloadLocalResources = (resArr, destionation) => {
-  resArr.map((res) => {
-    axios.get(res)
+const dowloadLocalResources = (urlsArr, destination) => {
+  urlsArr.map((url) => {
+    axios.get(url)
       .then((response) => {
-
-        fs.writeFile(destionation, response.data);
+        const filename = getFilenameExtensionFromUrl(url);
+        fs.writeFile(path.join(destination, filename), response.data);
       })
       .catch((err) => console.log(err));
-    return res;
+    return url;
   });
 };
-*/
-const load = (webPageUrl, destionationFolder = '/../test') => {
-  const htmlPath = `${path.join(destionationFolder, createFilenameByUrl(webPageUrl))}.html`;
-  const filesPath = `${path.join(destionationFolder, createFilenameByUrl(webPageUrl))}_files`;
-  return axios.get(webPageUrl)
+
+const load = (webPageUrl, destinationFolder = '/../test') => {
+  const htmlPath = `${path.join(destinationFolder, createFilenameByUrl(webPageUrl))}.html`;
+  const filesPath = `${path.join(destinationFolder, createFilenameByUrl(webPageUrl))}_files`;
+  return axios({
+    method: 'get',
+    url: webPageUrl,
+  })
     .then((response) => {
-      fs.writeFile(htmlPath, response.data);
+      fs.writeFile(htmlPath, response.data, 'utf-8');
       fs.mkdir(filesPath);
     })
     .then(() => parseTags(htmlPath))
     .then((parsedLinks) => {
       const localLinks = parseURL(parsedLinks, new URL(webPageUrl));
-      // dowloadLocalResources(localLinks, filesPath);
+      dowloadLocalResources(localLinks, filesPath);
     })
     .catch((error) => console.log(error));
 };
